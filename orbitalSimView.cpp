8b5338ab494c31e49f6c598d73e0057b1e7761bc
Ale Heir <aheir@itba.edu.ar>
2022-03-09 02:04:04 -0300

    Feat: render de bodies. Va todo muy rapido con el timeStep default

--- orbitalSimView.cpp
+++ orbitalSimView.cpp
@@ -9,25 +9,39 @@
 
 #include "orbitalSimView.h"
 
-const char* getISODate(float currentTime) {
-    // Epoch: 2022-01-01
-    struct tm epoch_tm = { 0, 0, 0, 1, 0, 122 };
-    time_t epoch = mktime(&epoch_tm);
+const char *getISODate(float currentTime)
+{
+	// Epoch: 2022-01-01
+	struct tm epoch_tm = {0, 0, 0, 1, 0, 122};
+	time_t epoch = mktime(&epoch_tm);
 
-    // Convert epoch time to local time
-    time_t local_time = epoch + (time_t)currentTime;
+	// Convert epoch time to local time
+	time_t local_time = epoch + (time_t)currentTime;
 
-    // Print ISO date for local time
-    struct tm* local_tm = localtime(&local_time);
-    return TextFormat("Date: %04d-%02d-%02d", 1900 + local_tm->tm_year, local_tm->tm_mon + 1, local_tm->tm_mday);
+	// Print ISO date for local time
+	struct tm *local_tm = localtime(&local_time);
+	return TextFormat("Date: %04d-%02d-%02d", 1900 + local_tm->tm_year, local_tm->tm_mon + 1, local_tm->tm_mday);
 }
 
 void renderOrbitalSim3D(OrbitalSim *sim)
 {
-    // Your code goes here...
+	int i;
+
+	for(i = 0; i < sim->bodyNum; i++)
+	{
+		Vector3 position = Vector3Scale(sim->bodysArray[i]->position, 1E-11);
+		Color color = sim->bodysArray[i]->color;
+
+		DrawSphere(position,
+				   logf(sim->bodysArray[i]->radius) * 0.005F,
+				   color);
+		
+		DrawPoint3D(position, color);
+	}
 }
 
 void renderOrbitalSim2D(OrbitalSim *sim)
 {
-    // Your code goes here...
+	DrawFPS(0, 0);
+	DrawText(getISODate(time(NULL)), 0, 20, 10, GOLD);
 }
